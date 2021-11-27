9c4b5008c6181066e12a8e55441c27adc29f19a6
Keuri Castillo <keury900@gmail.com>
2021-11-27 11:46:47 -0400

    hasta donde mostre

---
+++ ManejoDeEmpleados/ManejoDeEmpleados/Pages/log/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ManejoDeEmpleados.Pages.Login_y_Register
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
